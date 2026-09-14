8fbe23358e8610bfca0ebeb649595010c2386c2d
azerr-44630 <nerbalakerimov31333@gmail.com>
2026-09-14 10:32:02 +0400

    Chore: add .gitignore and remove cache/temporary test files

--- test_modern.cpp
+++
@@ -1,9 +0,0 @@
-#include <cstdlib>
-#include <memory>
-
-void process() {
-    double temp = 36.6;
-    int int_temp = static_cast<int>(temp);
-
-    char* buffer = std::make_unique<char[]>(256);
-}
